bdd91cac5316bc9d6dd1e712fc09496433df6ac5
권용환 <hahahaa_a@naver.com>
2023-04-22 22:05:18 +0900

    CoupleImage 위젯 생성

--- lib/screen/HomeScreen.dart
+++ lib/screen/HomeScreen.dart
@@ -18,3 +18,11 @@ class _DDay extends StatelessWidget {
     return Text('DDay Widget');
   }
 }
+
+class _CoupleImage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Couple Image Widget');
+  }
+}
